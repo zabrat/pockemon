811bf31856dc661f27c4517200ef4587c80d5c6b
zabrat <zaur1996@bk.ru>
2021-01-23 10:21:38 +0300

    don't work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import rootReducer from './roots/rootReducer/rootReducer';
 import { Provider } from 'react-redux';
@@ -25,8 +24,3 @@ function init() {
 }
 
 init();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
